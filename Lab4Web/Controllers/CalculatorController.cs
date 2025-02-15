9faaa761861998db0284a8e3244667c8d96ad693
Semiys <Semiysgladiys@gmail.com>
2025-02-15 20:28:42 +0400

    Update

---
+++ Lab4Web/Controllers/CalculatorController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lab4Web.Models;
+namespace Lab4Web.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new Calculate());
+        }
+        public IActionResult History()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ViewResult CalculateForm()
+        {
+            return View(new Calculate());
+        }
+
+        [HttpPost]
+        public ViewResult CalculateForm(Calculate calculationModel, string action)
+        {
+            if (action == "Очистить")
+            {
+                ModelState.Clear();
+                return View("Index", new Calculate());
+
+            }
+            if (!ModelState.IsValid)
+            {
+                return View("Index", calculationModel);
+            }
+            if (calculationModel.operand1.HasValue)
+            {
+                switch (calculationModel.operation)
+                {
+                    case "+":
+                        calculationModel.result = calculationModel.operand1.Value + calculationModel.operand2;
+                        break;
+                    case "-":
+                        calculationModel.result = calculationModel.operand1.Value - calculationModel.operand2;
+                        break;
+                    case "*":
+                        calculationModel.result = calculationModel.operand1.Value * calculationModel.operand2;
+                        break;
+                    case "/":
+                        calculationModel.result = calculationModel.operand1.Value / calculationModel.operand2;
+                        break;
+                    
+                    
+                }
+				string operationString = $"{calculationModel.operand1}{calculationModel.operation}{calculationModel.operand2}={calculationModel.result}";
+				var cookieOptions = new CookieOptions
+				{
+					Expires = DateTime.Now.AddMinutes(30)
+				};
+				Response.Cookies.Append("LastOperation", operationString, cookieOptions);
+			}
+            ViewBag.CheckValue = 100;// допустим равен ли результат 100
+            return View("Index",calculationModel);
+
+        }
+    }
+}
---
+++ Lab4Web/Models/calculate.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab4Web.Models
+{
+    public class Calculate
+    {
+        [Required(ErrorMessage="Пожалуйста, введите первое число")]
+        [Display(Name ="Первое число")]
+        public long? operand1 { get; set; }
+        [Range(1,10000,ErrorMessage="Пожалуйста, введите второе число в диапозоне от 1 до 10000")]
+        [Display(Name ="Второе число")]
+        public float operand2 { get; set; }
+        [Display(Name ="Операция")]
+        public string operation { get; set; }
+
+        [Display(Name ="Результат")]
+        public float result { get; set; }
+
+    }
+}
